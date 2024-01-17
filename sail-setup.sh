2fcfa3c5d639589756845aeba76dae5ce3bb0f7e
Alexandre Gomes <xandergomes@gmail.com>
2024-01-17 17:07:54 -0300

    first commit

---
+++ sail-setup.sh
@@ -0,0 +1,8 @@
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php83-composer:latest \
+    composer install --ignore-platform-reqs
+
+./vendor/bin/sail up -d
